5132df1882465494e2940a98361f4e48e5c8acdd
ziplock1337 <ziplock1337@gmail.com>
2024-10-10 00:30:14 +0300

    first test script

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function clockTimer() {
     const date = new Date();
+    const hour = string(now.getHours()).padStart(2, "0");
+    const minutes = string(now.getMinutes()).padStart(2, "0");
+    const seconds = string(now.getSeconds()).padStart(2, "0");
 
+    const times = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = times;
 }
+
+setInterval(clockTimer, 1000);
+
+clockTimer();
